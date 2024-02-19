a5b2e55b789ea19421b034a1159c7f63f09f2c87
Matthew32 <mmartinezmateo93@gmail.com>
2024-02-19 23:41:29 +0100

    Added port on .env

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { join } from 'node:path';
 import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
+  const port = process.env.PORT || 3001;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   app.useStaticAssets(join(__dirname, '/../public'));
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
